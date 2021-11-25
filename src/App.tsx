0047819550291d021a915b78cda712a80aef36e9
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-25 18:23:53 +0900

    適当に修正したら LabeledHeatmapが表示できた

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "../node_modules/react-vis/dist/style.css";
 
 import { NormalPlot } from "./NormalPlot";
 import { ShowHeatmap } from "./ShowHeatmap";
+import { LabeledHeatmap } from "./LabeledHeatmap";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <NormalPlot />
         <ShowHeatmap />
+        <LabeledHeatmap />
       </div>
     );
   }
